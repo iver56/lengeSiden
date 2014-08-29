9aee153e046436f8d47d6c68b16764a6eb937711
iver56 <iver56@hotmail.com>
2014-08-30 01:34:35 +0200

    Add new item modal. Show items in a list.

--- lengeSiden/www/js/controllers.js
+++ lengeSiden/www/js/controllers.js
@@ -1,14 +1,39 @@
-lengeSidenApp.controller("ListCtrl", function($scope, $firebase) {
-  $scope.newMessage = {text: ""};
+lengeSidenApp.controller("ListCtrl", function($scope, $firebase, $ionicModal) {
+  $ionicModal.fromTemplateUrl('templates/new-item-modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  $scope.newItem = {
+    name: "",
+    interval: 7 //number of days
+  };
+
   var ref = new Firebase(firebaseUrl);
   var sync = $firebase(ref);
-  $scope.messages = sync.$asArray();
+  $scope.items = sync.$asArray();
 
-  $scope.addItem = function(name) {
-    $scope.messages.$add($scope.newMessage);
+  $scope.openModal = function() {
+    $scope.modal.show();
   };
 
-  $scope.showModal = function() {
-    console.log('show modal');
+  $scope.saveNewItem = function() {
+    $scope.items.$add($scope.newItem);
+    $scope.modal.hide();
   };
+
+  $scope.intervals = [
+    {days:0.5, name: "To ganger hver dag"},
+    {days:1, name: "Hver dag"},
+    {days:2, name: "Annenhver dag"},
+    {days:7, name: "Hver uke"},
+    {days:14, name: "Annenhver uke"},
+    {days:30, name: "Hver måned"},
+    {days:61, name: "Annenhver måned"},
+    {days:91, name: "Hvert kvartal"},
+    {days:182, name: "Hvert halvår"},
+    {days:365, name: "Hvert år"}
+  ]
 });
